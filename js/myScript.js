dc5889562d9c46a687f2d4909928605db0688127
Samir Mejia <samir04m@gmail.com>
2018-07-13 00:00:01 -0500

    Barra de navegacion

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+    $('.dropdown-button').dropdown({
+          hover: true,
+          constrainWidth: false,
+          belowOrigin: true,
+    });
+    
+    $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();
 
 });
